cac2e5f9a71759d44af230e8339b8e917ff7f196
noelnoely <becketovanuri@gmail.com>
2026-06-09 18:02:00 +0200

    09_create_api_project

---
+++ TodoWebBackend/API/Program.cs
@@ -0,0 +1,28 @@
+using BLL;
+using DAL;
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DevelopmentConnection");
+
+if (string.IsNullOrWhiteSpace(connectionString))
+{
+    throw new InvalidOperationException("Connection string \"DevelopmentConnection\" was not found");
+}
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddDataAccessLayer(connectionString);
+builder.Services.AddBusinessLogicLayer();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
