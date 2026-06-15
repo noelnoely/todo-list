9d4e03ddedb421250210dff33afd66eba49ebd93
noelnoely <becketovanuri@gmail.com>
2026-06-15 13:46:00 +0200

    12_Add_Swagger_UI

--- TodoWebBackend/API/Program.cs
+++ TodoWebBackend/API/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddOpenApi();
 builder.Services.AddDataAccessLayer(connectionString);
 builder.Services.AddBusinessLogicLayer();
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -22,6 +23,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
